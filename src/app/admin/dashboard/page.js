fad0721501ef35b9ef318f1ebd43a0aef3df495e
Abdul Muiz Khan <abdulmuizkhan8@gmail.com>
2024-11-15 09:43:31 +0100

    font change

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -39,18 +39,18 @@ export default function AdminDashboardPage() {
             </p>
             {showCards && (
                 <div className={styles.cardContainer}>
-                    <a href="/admin/orders" className={styles.cardLink}>
-                        <div className={styles.card}>
-                            <img src="/images/ordini.png" alt="Gestione Ordini" className={styles.cardIcon} />
-                            <h2 className={styles.cardTitle}>Gestione Ordini</h2>
-                            <p>Visualizza e gestisci gli ordini dei clienti.</p>
-                        </div>
-                    </a>
                     <a href="/admin/inventory" className={styles.cardLink}>
                         <div className={styles.card}>
-                            <img src="/images/magazzino.png" alt="Gestione Magazzino" className={styles.cardIcon} />
+                            <img src="/images/magazzino.png" alt="Gestione Magazzino" className={styles.cardIcon}/>
                             <h2 className={styles.cardTitle}>Gestione Magazzino</h2>
                             <p>Monitora e aggiorna l'inventario dei prodotti.</p>
+                        </div>
+                    </a>
+                    <a href="/admin/orders" className={styles.cardLink}>
+                        <div className={styles.card}>
+                            <img src="/images/ordini.png" alt="Gestione Ordini" className={styles.cardIcon}/>
+                            <h2 className={styles.cardTitle}>Gestione Ordini</h2>
+                            <p>Visualizza e gestisci gli ordini dei clienti.</p>
                         </div>
                     </a>
                 </div>
